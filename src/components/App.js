5ce7fd2fe3d3652e24c5d3ca08419d23a156aa29
Jules Terrien <julesterrien@gmail.com>
2018-03-12 12:08:07 -0400

    use ENDPOINTS.users when accessing users endpoint

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { func, bool } from 'prop-types';
 import { getUsers, addUser } from '../actions';
+import { ENDPOINTS } from '../constants';
 import Loader from './Loader';
 import Users from './Users';
 
@@ -45,7 +46,7 @@ const mapState = state => ({
 const mapDispatch = dispatch => ({
 	dispatch,
 	onMount() {
-		dispatch(getUsers({ endpoint: 'users' }));
+		dispatch(getUsers({ endpoint: ENDPOINTS.users }));
 	},
 });
 
